6d9b0bbbccfa139e79d437b47e3e7a707bd3184e
Emily A GrasX <emily@gras.org>
2017-08-21 21:47:54 +0200

    @emilyagras #000 Do action on button click in sorta the right way of doing stuff

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,8 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
+import styles from './WelcomePage.css';
+
 
 class WelcomePage extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+    this.nextRoom = this.nextRoom.bind(this);
+  }
+
+  nextRoom (room) {
+    console.log(room);
+  }
   render() {
+    const firstDungeonRoom = "hello world";
+
     return (
       <div className="App">
         <div className="App-header">
@@ -12,10 +26,20 @@ class WelcomePage extends Component {
           {"This toy project was designed to help teach me how to use React so I don't cry about work so often. It's basically a text adventure game or something like Escape From Monkey Island/Sam and Max. Let's get right to it, then, shall we?"}
         </p>
         <input type="text" id="heroname" name="heroname" placeholder="Name your hero" />
-        <button class="StartJourney">Begin adventure!</button>
+        {startJourneyButton(firstDungeonRoom)}
       </div>
     )
   }
 }
+
+const startJourneyButton = (nextRoom) => (
+  <button
+    className={styles.startJourneyButton} onClick={() => {
+      console.log(nextRoom)
+  }}>
+    Begin adventure!
+  </button >);
+
+
 
 export default WelcomePage;
